2568d27dd1d6c0de29e2ed51e7976a9ceacd9e99
evkuzmina <ev-zakharova@mail.ru>
2022-02-26 18:37:59 +0500

    Ready Task_Final

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,18 @@
 //    }
 // }
 function sendRequest(name, phone, address, goods, sum) {
-
-    let data = {client: [], order: {address: []}, sum: [], goods: [ {title: [], count: [] }] };
+    let data = {goods: [], order: {}};
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods[i].count=goods[i].count;
-        data.goods[i].title=goods[i].title;
-
+        data.goods[i]= {title:goods[i].title, count:goods[i].count};
     }
 
     data.client = name + ' ' + phone;
     data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
     data.order.sum = sum;
    
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data: data});
 
-    return jsonData.data;
+    return jsonData;
 }
